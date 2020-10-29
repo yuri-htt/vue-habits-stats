f2ea75c4965cba3799d960b395035b309e3b090e
Yuri <yuri.swift.615@gmail.com>
2020-10-29 17:21:21 +0900

    ✨ ナビゲーションコンポーネントの作成

---
+++ src/components/organisms/NavigationBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="navigation">
+  </header>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+export default class NavigationBar extends Vue {}
+</script>
+
+<style scoped>
+.navigation{
+  width: 100%;
+  height: 54px;
+  background-color: #535353;
+}
+</style>
