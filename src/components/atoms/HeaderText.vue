8b3d7542d025be6d4f4abe2674aa7531ffb059b8
Yuri <yuri.swift.615@gmail.com>
2020-10-29 17:21:44 +0900

    ✨ ヘッダーテキストコンポーネントの作成

---
+++ src/components/atoms/HeaderText.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ value }}</h1>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class HeaderText extends Vue {
+  @Prop() private value!: string;
+}
+</script>
+
+<style scoped>
+h1 {
+  size: 32px;
+  text-align: left;
+}
+</style>
