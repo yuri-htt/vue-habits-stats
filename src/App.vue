5ad5d3119ab83dcb6df3de9757a884b80c04a5c1
Yuri <yuri.swift.615@gmail.com>
2020-10-29 17:23:32 +0900

    💄 HOME画面にコンポーネントを配置

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <NavigationBar />
+    <div class="container">
+      <HeaderText value="トータルスタッツ" />
+      <AggregateAtatsCard
+        colorType="forest"
+        :values="values"
+      />
+      <HeaderText value="個別スタッツ" />
+      <div class="card-list">
+        <div v-for="habit in habits" :key="habit.title" class="individual-card">
+          <IndividualStatsCard
+            :values="habit.values"
+            :title="habit.title"
+            :colorType="habit.colorType"
+          />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,12 +25,52 @@
 import { Component, Vue } from 'vue-property-decorator'
 import 'normalize.css'
 
-import HelloWorld from './components/HelloWorld.vue'
-import NavigationBar from './components/organisms/NavigationBar/index.vue'
+import NavigationBar from './components/organisms/NavigationBar.vue'
+import HeaderText from './components/atoms/HeaderText.vue'
+import AggregateAtatsCard from './components/organisms/AggregateStatsCard.vue'
+import IndividualStatsCard from './components/organisms/IndividualStatsCard.vue'
 
 @Component({
   components: {
-    HelloWorld
+    NavigationBar,
+    HeaderText,
+    AggregateAtatsCard,
+    IndividualStatsCard
+  },
+
+  data () {
+    return {
+      values: [
+        { '2020-10-04': 1 },
+        { '2020-10-25': 2 },
+        { '2020-10-27': 1 }
+      ],
+      habits: [
+        {
+          title: '習慣１',
+          colorType: 'spring',
+          values: [
+            { '2020-10-04': 1 },
+            { '2020-10-14': 2 },
+            { '2020-10-15': 2 },
+            { '2020-10-25': 2 },
+            { '2020-10-26': 1 },
+            { '2020-10-29': 2 }
+          ]
+        },
+        {
+          title: '習慣２',
+          colorType: 'ocean',
+          values: [
+            { '2020-10-06': 1 },
+            { '2020-10-11': 2 },
+            { '2020-10-20': 2 },
+            { '2020-10-25': 2 },
+            { '2020-10-26': 1 }
+          ]
+        }
+      ]
+    }
   }
 })
 export default class App extends Vue {}
@@ -27,6 +83,21 @@ export default class App extends Vue {}
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.container {
+  padding: 32px;
+}
+.card-list {
+  display: flex;
+  margin-bottom: 16px;
+}
+.card-list:last-child {
+  margin-bottom: 0;
+}
+.individual-card {
+  margin-right: 16px;
+}
+.individual-card:last-child {
+  margin-right: 0;
 }
 </style>
