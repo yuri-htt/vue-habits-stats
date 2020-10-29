6663d5e6bfb9d0d02a3ad43244c5f1657c1c5b9e
Yuri <yuri.swift.615@gmail.com>
2020-10-29 17:22:49 +0900

    ✨ 個別スタッツカードの作成

---
+++ src/components/organisms/IndividualStatsCard.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="card">
+    <p class="title">{{ title }}</p>
+    <div class="heat-map-container">
+      <HeatMap
+        :values="values"
+        :colorType="colorType"
+        :vertical="true"
+        size="regular"
+        verical="true"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+
+import dayjs from 'dayjs'
+import HeatMap from '../molecules/HeatMap.vue'
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component({
+  components: {
+    HeatMap
+  }
+})
+
+export default class IndividualStatsCard extends Vue {
+  @Prop({ default: 'forest' }) private colorType!: 'forest' | 'spring' | 'ocean' | 'mican' | 'tomato'
+  @Prop() private values!: {[key: string]: number }
+  @Prop() private title!: string
+
+  private weeksCountForDisplay = 4
+  private dateArray: {[key: string]: number }[] = []
+  private today!: dayjs.Dayjs
+  private startDate!: dayjs.Dayjs
+
+  data () {
+    return {
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+.card {
+  border: 1px solid #EAEAEA;
+  box-sizing: border-box;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 4px;
+  padding: 16px
+}
+.title {
+  font-size: 16px;
+  font-weight: 700;
+  text-align: left;
+}
+
+.heat-map-container {
+
+}
+</style>
